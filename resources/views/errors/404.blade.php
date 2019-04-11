e4ef43da30e3296f55c7cb4a2fd5335ab257ab02
GreenWhite <greenwhitedev@gmail.com>
2019-04-12 00:13:31 +0100

    Add first error page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>404 - {{config('app.name')}}</title>
+    <meta name="description" content="Dashmix - Bootstrap 4 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest | This is the demo of Dashmix! You need to purchase a license for legal use! | DEMO">
+    <meta name="author" content="pixelcave">
+    <meta name="robots" content="noindex, nofollow">
+    <meta property="og:title" content="{{config('app.name')}}">
+    <meta property="og:site_name" content="{{config('app.name')}}">
+    <meta property="og:description" content="{{config('app.name')}}">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="">
+    <meta property="og:image" content="">
+    <link rel="shortcut icon" href="/png/favicon.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="/png/favicon-192x192.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/png/apple-touch-icon-180x180.png">    <link rel="stylesheet" href="/css/css1f05.css?family=Nunito+Sans:300,400,400i,600,700">
+    <link rel="stylesheet" id="css-main" href="/css/dashmix.min-1.1.css">
+    <script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','js/analytics.js','ga');ga('create', 'UA-16158021-6', 'auto');ga('send', 'pageview');</script>
+</head>
+<body>
+<div id="page-container">
+    <main id="main-container">
+        <div class="bg-image" style="background-image: url('jpg/photo19%402x.jpg');">
+            <div class="hero bg-white-95">
+                <div class="hero-inner">
+                    <div class="content content-full">
+                        <div class="px-3 py-5 text-center">
+                            <div class="row invisible" data-toggle="appear">
+                                <div class="col-sm-6 text-center text-sm-right">
+                                    <div class="display-1 text-danger font-w700">404</div>
+                                </div>
+                                <div class="col-sm-6 text-center d-sm-flex align-items-sm-center">
+                                    <div class="display-1 text-muted font-w300">Error</div>
+                                </div>
+                            </div>
+                            <h1 class="h2 font-w700 mt-5 mb-3 invisible" data-toggle="appear" data-class="animated fadeInUp" data-timeout="300">Oops.. There's no page for you here..</h1>
+                            <h2 class="h3 font-w400 text-muted mb-5 invisible" data-toggle="appear" data-class="animated fadeInUp" data-timeout="450">We are sorry, you wandered too far..</h2>
+                            <div class="invisible" data-toggle="appear" data-class="animated fadeInUp" data-timeout="600">
+                                <a class="btn btn-hero-secondary" href="{{url()->previous()}}">
+                                    <i class="fa fa-arrow-left mr-1"></i> Retrace your Steps
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</div>
+<script src="/js/dashmix.core.min-1.1.js"></script>
+<script src="/js/dashmix.app.min-1.1.js"></script>
+</body>
+
+<!-- Mirrored from demo.pixelcave.com/dashmix/op_error_404.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 30 May 2018 16:47:15 GMT -->
+</html>
