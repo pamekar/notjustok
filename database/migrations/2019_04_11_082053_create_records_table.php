fd80a5f1b5beeaa165cd6bf0cc3366f514b9ce73
GreenWhite <greenwhitedev@gmail.com>
2019-04-11 10:04:10 +0100

    Add Database Seeders and Migrations

---
+++ database/migrations/2019_04_11_082053_create_records_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRecordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('records', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('upload_id');
+            $table->string('isrc');
+            $table->string('artist');
+            $table->string('product_title');
+            $table->string('contrainer_title');
+            $table->string('label');
+            $table->string('country');
+            $table->integer('total_plays');
+            $table->integer('revenue');
+            $table->timestamp('start_at')->nullable();
+            $table->timestamp('end_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('records');
+    }
+}
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $faker = \Faker\Factory::create();
+        \App\User::create([
+            'name'              => 'user',
+            'email'             => 'user@site.com',
+            'password'          => bcrypt('password'),
+            'remember_token'    => \Illuminate\Support\Str::random(60),
+            'email_verified_at' => date('Y-m-d')
+        ]);
+    }
+}
