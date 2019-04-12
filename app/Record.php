2f1a10207edf9f773f05b4aaf4c8adc9997ac6cd
GreenWhite <greenwhitedev@gmail.com>
2019-04-13 00:48:30 +0100

    Added $fillable columns for mass assignment

--- app/Record.php
+++ app/Record.php
@@ -7,4 +7,24 @@ use Illuminate\Database\Eloquent\Model;
 class Record extends Model
 {
     //
+    protected $fillable
+        = [
+            'upload_id',
+            'user_id',
+            'isrc',
+            'artist',
+            'product_title',
+            'container_title',
+            'label',
+            'country',
+            'total_plays',
+            'revenue',
+            'start_at',
+            'end_at'
+        ];
+
+    public function upload()
+    {
+        return $this->belongsTo('App\Upload');
+    }
 }
--- app/Upload.php
+++ app/Upload.php
@@ -7,4 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Upload extends Model
 {
     //
+    protected $fillable = ['user_id', 'title', 'batch_code','description', 'row_count'];
 }
