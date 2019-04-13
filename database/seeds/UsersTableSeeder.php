3bf907dc7c7cd4f216f0291dcc7b79cf6d5c51ff
GreenWhite <greenwhitedev@gmail.com>
2019-04-13 02:48:25 +0100

    remove $faker. 'Faker/Factory' not found error

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,7 +12,7 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         //
-        $faker = \Faker\Factory::create();
+        //$faker = \Faker\Factory::create();
         \App\User::create([
             'name'              => 'user',
             'email'             => 'user@site.com',
